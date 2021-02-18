17a45fbbc19c4a739ade07ca754576f5d2fefa64
Mike Palmer <mrpalmer685@gmail.com>
2021-02-18 16:47:58 -0500

    Add initial git clone logic

--- lib/dottie/git.rb
+++ lib/dottie/git.rb
@@ -2,6 +2,15 @@
 
 module Dottie
   class Git
-    def clone(repo, location); end
+    def initialize(shell = Dottie::Shell.new)
+      @shell = shell
+    end
+
+    def clone(repo, location)
+      args = ['--depth 1']
+      args << "-b #{repo.branch}" unless repo.branch.nil?
+      args << "-b #{repo.tag}" unless repo.tag.nil?
+      @shell.run('git clone', *args, repo.url, location)
+    end
   end
 end
